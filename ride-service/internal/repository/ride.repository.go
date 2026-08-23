7fc9bd988f52d0b409cd3feb60f559ca78b6091c
Abhishek lohia <abhisheklohia@tripxl.com>
2026-08-23 13:23:55 +0530

    add ride repository methods"

--- ride-service/internal/repository/ride.repository.go
+++ ride-service/internal/repository/ride.repository.go
@@ -12,6 +12,25 @@ type Repository interface {
 		ctx context.Context,
 		req models.Ride,
 	) (*models.Ride, error)
+
+	CreateRideStatusHistory(
+		ctx context.Context,
+		tx *gorm.DB,
+		history models.RideStatusHistory,
+	) error
+
+	GetRideByID(
+		ctx context.Context,
+		tx *gorm.DB,
+		id uint64,
+	) (*models.Ride, error)
+
+	UpdateRideStatus(
+		ctx context.Context,
+		tx *gorm.DB,
+		rideID uint64,
+		status string,
+	) error
 }
 
 type repositoryImpl struct {
@@ -38,4 +57,45 @@ func (r repositoryImpl) CreateRide(
 	}
 
 	return &req, nil
+}
+
+func (r repositoryImpl) CreateRideStatusHistory(
+	ctx context.Context,
+	tx *gorm.DB,
+	history models.RideStatusHistory,
+) error {
+	return tx.WithContext(ctx).
+		Create(&history).
+		Error
+}
+
+func (r repositoryImpl) GetRideByID(
+	ctx context.Context,
+	tx *gorm.DB,
+	id uint64,
+) (*models.Ride, error) {
+
+	var ride models.Ride
+
+	if err := tx.WithContext(ctx).
+		First(&ride, id).
+		Error; err != nil {
+		return nil, err
+	}
+
+	return &ride, nil
+}
+
+func (r repositoryImpl) UpdateRideStatus(
+	ctx context.Context,
+	tx *gorm.DB,
+	rideID uint64,
+	status string,
+) error {
+
+	return tx.WithContext(ctx).
+		Model(&models.Ride{}).
+		Where("id = ?", rideID).
+		Update("status", status).
+		Error
 }
